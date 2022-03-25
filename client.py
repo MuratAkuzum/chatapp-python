e125120f57af03b2f0a81fc69de14ca41e3faa3f
Murat Akuzum <mrtkzm@gmail.com>
2022-03-25 15:04:22 +0000

    second commit

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
-import socket
+import socket, threading
 
 HOST = "127.0.0.1"
-PORT = 8888
+PORT = 12345
 SIZE = 1024
 FORMAT = 'utf-8'
 
@@ -9,12 +9,37 @@ def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((HOST, PORT))
 
-    while True:
-        
-        data = input(">")
-        data_sent = server.send(data.encode(FORMAT))
+    def receive():
+        while True:
+            data_receive = server.recv(SIZE).decode(FORMAT)
+            print(f"Server sends: {data_receive}")
 
-    server.close()
+    def send():
+        while True:
+            data = input("> ")
+            server.send(data.encode(FORMAT))
+        
+
+    receive_thread = threading.Thread(target=receive)
+    receive_thread.start()
+
+    send_thread = threading.Thread(target=send)
+    send_thread.start()
 
 if __name__ == '__main__':
     main()
+
+
+
+    # def send():
+    #     while True:
+    #         data = input("> ")
+
+    #         if data == "/quit":
+    #             print("Closing connection...")
+    #             server.close()
+    #             return
+    #         elif data:
+    #             server.send(data.encode(FORMAT))
+    #         else:
+    #             return    
