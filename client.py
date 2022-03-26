6448f4e2df12f4da9b2b3b0624e613384a8e7ea2
Murat Akuzum <mrtkzm@gmail.com>
2022-03-26 15:21:24 +0000

    clean up - mainly client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import socket, threading
+import socket, threading, sys
 
 HOST = "127.0.0.1"
 PORT = 12345
@@ -9,37 +9,41 @@ def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((HOST, PORT))
 
-    def receive():
-        while True:
-            data_receive = server.recv(SIZE).decode(FORMAT)
-            print(f"Server sends: {data_receive}")
-
     def send():
         while True:
-            data = input("> ")
-            server.send(data.encode(FORMAT))
-        
+            try:
+                data = input("> ")
+                server.send(data.encode(FORMAT))
+            except KeyboardInterrupt:
+                server.close()
+                sys.exit()
 
-    receive_thread = threading.Thread(target=receive)
+    def receive():
+        while True:
+            try: 
+                data_receive = server.recv(SIZE).decode(FORMAT)
+                if len(data_receive) == 0:
+                    server.shutdown(socket.SHUT_RDWR)
+                    server.close()
+                    break
+                print(f"Server sends: {data_receive}")
+            except:
+                break
+
+        sys.exit()
+
+    send_thread = threading.Thread(target=send, daemon=True)
+    send_thread.start()
+    
+    receive_thread = threading.Thread(target=receive, daemon=True)
     receive_thread.start()
 
-    send_thread = threading.Thread(target=send)
-    send_thread.start()
+    receive()
+
+    server.shutdown(socket.SHUT_RDWR)
+    server.close()
+    sys.exit()
 
 if __name__ == '__main__':
     main()
 
-
-
-    # def send():
-    #     while True:
-    #         data = input("> ")
-
-    #         if data == "/quit":
-    #             print("Closing connection...")
-    #             server.close()
-    #             return
-    #         elif data:
-    #             server.send(data.encode(FORMAT))
-    #         else:
-    #             return    
--- server.py
+++ server.py
@@ -23,41 +23,57 @@ class Client:
             if username not in client_list:
                 Client.username = username
                 Client.online = True
-                client_list[Client.username] = Client.client_socket.getpeername()[0]
+                client_list[Client.username] = Client.client_socket
                 print(f"Username: {Client.username} \nAddress: {Client.client_socket.getpeername()[0]}")
                 print(f"{client_list}")
                 Client.connected = True
                 break
             else:
                 Client.client_socket.send("Username is already taken, please enter another name:".encode(FORMAT))
-                
+    
+    def client_logoff():
+        try:
+            Client.online = False
+            Client.connected = False
+            client_list.pop(Client.username, Client.client_socket)
+            Client.client_socket.close()
+        except:
+            print("Error while trying to log off the client!")
 
     def handle_client(client_socket, client_addr):
-        print(f"New client connected with {Client.client_socket.getpeername()[0]}!")
-
+        print(f"{client_list}")
         Client.client_login()
+
+        print(f"{Client.username} connected with {Client.client_socket.getpeername()[0]}!")
+
 
         while Client.connected:
             data_received = Client.client_socket.recv(SIZE).decode(FORMAT)
             print(f"{Client.username} sends: {data_received}")
 
             if len(data_received) == 0:
-                print(f"{Client.username} has just left the server!")
-                Client.online = False
-                Client.connected = False
-                client_list.pop(Client.username)
+                Client.client_socket.send("Exit request received, closing connection...".encode(FORMAT))
+                Client.client_logoff()
+                print(f"{client_list}")
                 break
-            elif data_received == "/quit":
-                print(f"{Client.username} has just left the server!")
-                Client.online = False
-                Client.connected = False
-                client_list.pop(Client.username)
-                break
-            else:
-                pass
 
-        if not Client.connected:
+            elif data_received == "/quit":
+                broadcast("Someone has just left the server!")
+                Client.client_socket.send("Exit request received, closing connection...".encode(FORMAT))
+                Client.client_logoff()
+                print(f"{client_list}")
+                break
+
+            else:
+                continue
+
+        if Client.connected == False:
             Client.client_socket.close()
+
+def broadcast(message):
+    message = ""
+    for client in client_list.values():
+        client.send(message.encode(FORMAT))
 
 def run_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -66,30 +82,24 @@ def run_server():
         server.listen()
         print(f"Server started listening on {HOST}:{PORT} for client connection...")
     except:
-        server.shutdown(1)
-        server.close()
         print("Server failed to start, try again!")
-        return 1
+        # server.shutdown(socket.SHUT_RDWR)
+        server.close()
 
     while True:
         Client.client_socket, Client.client_addr = server.accept()
         
-        thread = threading.Thread(target=Client.handle_client, args=(Client.client_socket, Client.client_addr))
-        thread.start()
-        print(f"Current active connections: {threading.active_count() -1}")
+        with Client.client_socket:
+            thread = threading.Thread(target=Client.handle_client, args=(Client.client_socket, Client.client_addr))
+            thread.start()
+            print(f"Current active connections: {threading.active_count() -1}")
 
-
-# def broadcast(message):
-#     message = input("")
-#     for client in :
-#         client.send(message.encode(FORMAT))
+    server.shutdown(socket.SHUT_RDWR)
+    server.close()
 
 def main():
-    run_server()   
-
+    run_server()
 
 if __name__ == '__main__':
     main()
-
-
 
