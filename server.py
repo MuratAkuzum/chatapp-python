8ffd3635cf4258c82778b58ade428f7eae6e6af5
Murat Akuzum <mrtkzm@gmail.com>
2022-03-26 21:39:39 +0000

    cleanup server.py

--- server.py
+++ server.py
@@ -7,92 +7,73 @@ FORMAT = 'utf-8'
 
 client_list = {}
 
-class Client:
-    def __init__(self, username, client_addr, client_socket, online, connected):
-        self.username = username
-        self.client_addr = client_addr
-        self.client_socket = client_socket
-        self.online = bool
-        self.connected = bool
-
-    def client_login():
-        Client.client_socket.send("Welcome to the server. Please enter a username:".encode(FORMAT))
-
-        while True:
-            username = Client.client_socket.recv(SIZE).decode(FORMAT)
-            if username not in client_list:
-                Client.username = username
-                Client.online = True
-                client_list[Client.username] = Client.client_socket
-                print(f"Username: {Client.username} \nAddress: {Client.client_socket.getpeername()[0]}")
-                print(f"{client_list}")
-                Client.connected = True
-                break
-            else:
-                Client.client_socket.send("Username is already taken, please enter another name:".encode(FORMAT))
-    
-    def client_logoff():
-        try:
-            Client.online = False
-            Client.connected = False
-            client_list.pop(Client.username, Client.client_socket)
-            Client.client_socket.close()
-        except:
-            print("Error while trying to log off the client!")
-
-    def handle_client(client_socket, client_addr):
-        print(f"{client_list}")
-        Client.client_login()
-
-        print(f"{Client.username} connected with {Client.client_socket.getpeername()[0]}!")
-
-
-        while Client.connected:
-            data_received = Client.client_socket.recv(SIZE).decode(FORMAT)
-            print(f"{Client.username} sends: {data_received}")
-
-            if len(data_received) == 0:
-                Client.client_socket.send("Exit request received, closing connection...".encode(FORMAT))
-                Client.client_logoff()
-                print(f"{client_list}")
-                break
-
-            elif data_received == "/quit":
-                broadcast("Someone has just left the server!")
-                Client.client_socket.send("Exit request received, closing connection...".encode(FORMAT))
-                Client.client_logoff()
-                print(f"{client_list}")
-                break
-
-            else:
-                continue
-
-        if Client.connected == False:
-            Client.client_socket.close()
-
 def broadcast(message):
-    message = ""
     for client in client_list.values():
         client.send(message.encode(FORMAT))
 
+def client_login(client_socket):
+        client_socket.send("Welcome to the server. Please enter a username:".encode(FORMAT))
+
+        while True:
+            username = client_socket.recv(SIZE).decode(FORMAT)
+            if username not in client_list:
+                client_list[username] = client_socket
+                print(f"Username: {username} \nAddress: {client_socket.getpeername()[0]}")
+                print(f"{client_list}")
+                break
+            else:
+                client_socket.send("Username is already taken, please enter another name:".encode(FORMAT))
+        return username
+
+def client_logoff(username, client_socket):
+        try:
+            client_list.pop(username, client_socket)
+            client_socket.close()
+        except:
+            print("Error while trying to log off the client!")
+
+def handle_client(client_socket, client_addr):
+        print(f"{client_list}")
+        username = client_login(client_socket)
+        print(f"{username} connected with {client_socket.getpeername()[0]}!")
+
+        while True:
+            data_received = client_socket.recv(SIZE).decode(FORMAT)
+            print(f"{username} sends: {data_received}")
+
+            if len(data_received) == 0:
+                client_socket.send("Exit request received, closing connection...".encode(FORMAT))
+                client_logoff(username, client_socket)
+                print(f"{client_list}")
+                break
+            elif data_received == "/quit":
+                broadcast(f"{username} has just left the server!")
+                client_socket.send("Exit request received, closing connection...".encode(FORMAT))
+                client_logoff(username, client_socket)
+                print(f"{client_list}")
+                break
+            elif data_received == "/who":
+                client_socket.send(f"{client_list.keys()}".encode(FORMAT))
+            else:
+                continue
+
 def run_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:    
         server.bind((HOST, PORT))
         server.listen()
         print(f"Server started listening on {HOST}:{PORT} for client connection...")
     except:
         print("Server failed to start, try again!")
-        # server.shutdown(socket.SHUT_RDWR)
+        server.shutdown(socket.SHUT_RDWR)
         server.close()
 
     while True:
-        Client.client_socket, Client.client_addr = server.accept()
-        
-        with Client.client_socket:
-            thread = threading.Thread(target=Client.handle_client, args=(Client.client_socket, Client.client_addr))
-            thread.start()
-            print(f"Current active connections: {threading.active_count() -1}")
+        client_socket, client_addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(client_socket, client_addr), daemon=True)
+        thread.start()
+        print(f"Current active connections: {threading.active_count() -1}")
 
     server.shutdown(socket.SHUT_RDWR)
     server.close()
@@ -103,3 +84,6 @@ def main():
 if __name__ == '__main__':
     main()
 
+
+# TODO: Clean up the names show up in /who broadcast message
+# TODO: Add more commands
