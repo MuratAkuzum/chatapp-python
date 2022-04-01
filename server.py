00b38f523c193988593dc2715b6863e58ca51b83
Murat Akuzum <mrtkzm@gmail.com>
2022-04-01 19:47:57 +0100

    Improved messages and started working on channels

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, threading
+import socket, threading, dataclasses
 
 HOST = "127.0.0.1"
 PORT = 12345
@@ -7,12 +7,20 @@ FORMAT = 'utf-8'
 
 client_list = {}
 
+@dataclasses
+class Channel:
+    name: str
+    members: list[str]
+    owner: str
+    co_owners: list[str]
+
+
 def broadcast(message):
     for client in client_list.values():
         client.send(message.encode(FORMAT))
 
 def client_login(client_socket):
-        client_socket.send("[SERVER]: Welcome to the server. Please enter a username:".encode(FORMAT))
+        client_socket.send("[SERVER]: Please enter a username to login to the server:".encode(FORMAT))
         while True:
             username = client_socket.recv(SIZE).decode(FORMAT)
             # TODO: add more filteration while picking a unique username (char limit, allowed special chars etc.)
@@ -23,6 +31,7 @@ def client_login(client_socket):
                 break
             else:
                 client_socket.send("[SERVER]: Username is already taken, please enter another name:".encode(FORMAT))
+        client_socket.send(f"[SERVER]: Welcome to the server. You have logged in as {username}.\n> Type </help> if you want to see options.".encode(FORMAT))
         return username
 
 def client_logoff(username, client_socket):
@@ -56,6 +65,9 @@ def handle_client(client_socket, client_addr):
                 client_logoff(username, client_socket)
                 break
 
+            elif data_received == "/help":
+                client_socket.send("[SERVER]: choose one of the following commands:\n> Type </quit> if you want to quit the program.\n> Type </send> <username> <your message> to send message to other users.".encode(FORMAT))
+
             # TODO: Users are not always getting listed always in the same order. Handle /who better.
             # TODO: Add option to search someone specifically : /who murat
             elif data_received == "/who":
@@ -75,7 +87,45 @@ def handle_client(client_socket, client_addr):
                     client_socket.send("[SERVER]: User is not online or does not exist!".encode(FORMAT))
                 else:
                     direct_message(username, other_username, ' '.join(message))
-                                
+
+            elif data_received.startswith("/channel"):
+                data_split = data_received.split(" ")
+                channel_name = data_split[1]
+                channel_command = data_split[2]
+                
+                
+                if channel_command == "create":
+                    # TODO: create channel if doesn't already exist:
+                    pass
+                elif channel_command == "join":
+                    # TODO: find the owner and ask permission to join the channel:
+                    pass
+                elif channel_command == "invite":
+                    channel_user = data_split[3]
+                    # TODO: invite the user to your channel if exists, if not, create channel if invite is accepted:
+                    pass
+                elif channel_command == "quit":
+                    # TODO: leave channel, if the user is last person in the channel, destroy the channel too:
+                    pass
+                elif channel_command == "kick":
+                    channel_user = data_split[3]
+                    # TODO: kick the user if used by the owner of the channel:
+                    pass
+                elif channel_command == "who":
+                    # TODO: list the users in the channel:
+                    # TODO: if the username specified, only give information about that user:
+                    channel_user = data_split[3]
+                    pass
+                elif channel_command == "send":
+                    # TODO: send message to specified channel name:
+                    pass
+                
+                else:
+                    pass
+
+    
+
+                    
             else:
                 continue
 
